cf198e3826a1faaa3d30a3157b226bf5816c7326
Stread <83221877+systm-spec@users.noreply.github.com>
2025-01-15 18:52:20 +0100

    Update SearchCustomer.py
    set up search fn,
    tested

--- src/modules/SearchCustomer.py
+++ src/modules/SearchCustomer.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+
 from src.controller.find import find_by_id
 
 class SearchCustomer(ctk.CTkFrame):
@@ -26,13 +27,24 @@ class SearchCustomer(ctk.CTkFrame):
         self.search_btn.grid(ipadx=3, ipady=4, column=1, row=0)
 
         # Content-Frame
-        self.content_frame = ctk.CTkFrame(self, fg_color="#808080")
+        self.content_frame = ctk.CTkFrame(self, fg_color="transparent")
         self.content_frame.grid(column=0, row=1)
-        self.test_lbl = ctk.CTkLabel(self.content_frame, text="placeholder")
-        self.test_lbl.grid(ipadx=4, ipady=4)
 
     # Fn um den search_var zu ändern und ihn zu casten
     def set_text(self):
         self.search_var_int = int(self.search_var.get())
         workload = find_by_id(self.search_var_int)
         print("--------------\n",workload["first_name"])
+        if not workload.empty:
+            self.render_fields(workload)
+        else:
+            print(workload, "RENDER STOPPED")
+
+    def render_fields(self, workload):
+        self.counter = 0
+        for item in workload.columns:
+            temp_lbl = ctk.CTkLabel(self.content_frame, text=item, bg_color="#3f3f46")
+            temp_lbl.grid(column=0, row=self.counter, sticky="nswe")
+            temp_content = ctk.CTkLabel(self.content_frame, text=workload[item].values[0], width=320, fg_color="#3f3f46")
+            temp_content.grid(column=1, row=self.counter)
+            self.counter += 1
