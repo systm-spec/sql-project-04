05093d85e1a4d065e3376044fe0cfb30ed30231f
Stread <83221877+systm-spec@users.noreply.github.com>
2025-01-15 17:57:09 +0100

    DB abfrage eingerichtet
    Query geschrieben, Button fn geschrieben und getestet.

--- src/controller/find.py
+++ src/controller/find.py
@@ -6,23 +6,22 @@ from sqlalchemy.orm import Session
 from sqlalchemy import text
 import psycopg
 
-from src.controller.connector import open_connection
-
 
 def find_by_id(item):
-    connection_url = f'postgresql://postgres:Datacraft@localhost:5432/hoteldb'
+    connection_url = f'postgresql://postgres:admin@localhost:5432/hoteldb'
     engine = create_engine(connection_url)
 
     # check connection:
     with engine.connect() as conn_alchemy:
         print("SQLAlchemy connected!")
 
-
     try:
         sel_stmt = f'SELECT * FROM meta WHERE "id" = {item};'
         complete_df = pd.read_sql(sel_stmt, engine)
-        print(complete_df)
+        # print("DataFrame loaded: \n", complete_df)
+        return complete_df
     except Exception as error:
         print(f'{error}')
 
-find_by_id(1)
+    print("Find End")
+
--- src/modules/SearchCustomer.py
+++ src/modules/SearchCustomer.py
@@ -1,8 +1,10 @@
 import customtkinter as ctk
+from src.controller.find import find_by_id
 
 class SearchCustomer(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
+
 
         # Grid-System
         self.grid_columnconfigure(0, weight=1)
@@ -14,11 +16,13 @@ class SearchCustomer(ctk.CTkFrame):
         self.search_frame.grid_columnconfigure(1, weight=1)
 
         # Search-Bar
-        self.search_entry = ctk.CTkEntry(self.search_frame, height=28, width=280)
+        self.search_var = ctk.StringVar()
+        self.search_entry = ctk.CTkEntry(self.search_frame, height=28, width=280, textvariable=self.search_var, placeholder_text="Suchbegriff eingeben")
         self.search_entry.grid(ipadx=3, ipady=4, column=0, row=0)
 
         # Search-Button
-        self.search_btn = ctk.CTkButton(self.search_frame, fg_color="transparent", corner_radius=2, text="Search")
+        self.search_btn = ctk.CTkButton(self.search_frame, fg_color="transparent", corner_radius=2, text="Search",
+                                        command=self.set_text)
         self.search_btn.grid(ipadx=3, ipady=4, column=1, row=0)
 
         # Content-Frame
@@ -26,3 +30,9 @@ class SearchCustomer(ctk.CTkFrame):
         self.content_frame.grid(column=0, row=1)
         self.test_lbl = ctk.CTkLabel(self.content_frame, text="placeholder")
         self.test_lbl.grid(ipadx=4, ipady=4)
+
+    # Fn um den search_var zu ändern und ihn zu casten
+    def set_text(self):
+        self.search_var_int = int(self.search_var.get())
+        workload = find_by_id(self.search_var_int)
+        print("--------------\n",workload["first_name"])
