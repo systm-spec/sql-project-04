b9ae2d35d97a120b79aa18116980b00ca6bbb9d7
Stread <83221877+systm-spec@users.noreply.github.com>
2025-01-16 09:49:00 +0100

    resize

--- src/app.py
+++ src/app.py
@@ -14,20 +14,20 @@ class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Hotel-App")
-        self.geometry("1280x720")
+        self.geometry("1280x900")
         ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("config/theme/custom.json")
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.bind("<Escape>", lambda a: self.destroy())
 
-        ## SearchCustomer ###
-        #self.start_menu_frame = SearchCustomer(master=self, fg_color="transparent")
-        #self.start_menu_frame.grid(row=0, column=0)
+        # SearchCustomer ###
+        self.start_menu_frame = SearchCustomer(master=self, fg_color="transparent")
+        self.start_menu_frame.grid(row=0, column=0)
 
-        #### CreateCustomer ###
-        self.start_character_frame = CreateCustomer(self, fg_color="transparent", width=820)
-        self.start_character_frame.grid(row=0, column=0, sticky="nswe")
+        ##### CreateCustomer ###
+        #self.start_character_frame = CreateCustomer(self, fg_color="transparent", width=820)
+        #self.start_character_frame.grid(row=0, column=0, sticky="nswe")
 
 
 # MAIN
